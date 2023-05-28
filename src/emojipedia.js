6ad3dbd00a829ea2fad0b5c6efdb800d70827781
Zsolt Marku <123772528+KipSter91@users.noreply.github.com>
2023-05-28 18:41:04 +0200

    Initial commit

---
+++ src/emojipedia.js
@@ -0,0 +1,45 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji: "😇",
+    name: "Smiling Face With Halo",
+    meaning:
+      "A heavenly smile is shown by the 😇 halo emoji, which symbolizes innocence and goodness. People use it to express their good deeds, loving behavior or exemplary attitude. Sometimes this emoji is used humorously to show a winking innocence for less good deeds or behaviors. It conveys kindness and goodwill and adds a playful touch to communication. You can use it to add charm and lightness to your messages or to round out a humorous comment."
+  },
+  {
+    id: 5,
+    emoji: "🙃",
+    name: "Upside-Down Face",
+    meaning:
+      "I'm not serious! The inverted smiling face 🙃 indicates that you are not serious or have a humorous undertone. It is suitable for ambiguous statements, ironic remarks or joking comments. Also, it can be used to express silliness or in response to a joke. In conversations, it emphasizes the playful nature of a message and helps clarify potential misunderstandings about the tone of the message. Additionally, the emoji emphasizes lightness and cheerfulness, even when the topic is serious. Finally, it is a creative way to create a relaxed atmosphere in text messages."
+  },
+  {
+    id: 6,
+    emoji: "😍",
+    name: "Smiling Face With Heart-Eyes",
+    meaning:
+      "A smile of love is shown by the 😍 emoji, which beams with happiness with its heart-shaped eyes. It conveys intense love and affection for a person, place or even an object. This emoji is often used in romantic messages to express love, gratitude, and admiration. It can also be used to convey enthusiasm for a cause or event. The heart-shaped eyes symbolize overwhelming feelings of happiness and a strong connection."
+  }
+];
+export default emojipedia;
